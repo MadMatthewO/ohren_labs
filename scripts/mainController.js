0b3aad33aa7e2443ab718147d30a898dc9806adb
MadMatthewO <44182998+MadMatthewO@users.noreply.github.com>
2019-05-14 20:52:23 -0500

    AngularJS implementation

---
+++ scripts/mainController.js
@@ -0,0 +1,22 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.htm",
+    })
+    .when("/workHistory", {
+        templateUrl : "/views/work_history.html",
+        controller : "workHistoryController"
+    })
+    .when("/schoolHistory", {
+        templateUrl : "/views/school_history.html",
+        controller : "schoolHistoryController"
+    });
+});
+app.controller("workHistoryController", function ($scope) {
+    $scope.msg = "I love London";
+});
+app.controller("schoolHistoryController", function ($scope) {
+    $scope.msg = "I love Paris";
+});
